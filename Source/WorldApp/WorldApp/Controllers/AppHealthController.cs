4e429f6cc771b90eecbfe0f90a9dd9f6df492f77
giglesias <gabriel.iglesias@southworks.net>
2012-09-25 08:33:14 -0700

    First commit

---
+++ Source/WorldApp/WorldApp/Controllers/AppHealthController.cs
@@ -0,0 +1,23 @@
+﻿namespace WorldApp.Controllers
+{
+    using System.Web.Mvc;
+    using Microsoft.WindowsAzure.ServiceRuntime;
+    using WorldApp.Services;
+
+    public class AppHealthController : Controller
+    {
+        public ActionResult Index()
+        {
+            string serviceUrlPrefix = RoleEnvironment.GetConfigurationSettingValue("HostedServiceUrlPrefix");
+
+            ServiceManager manager = new ServiceManager();
+            bool appIsOnline = manager.GetHostedServiceStatus(serviceUrlPrefix);
+            if (!appIsOnline)
+            {
+                Response.Close();
+            }
+            
+            return new EmptyResult();
+        }
+    }
+}
